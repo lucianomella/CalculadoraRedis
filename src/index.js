ab5705452c42c07390b44a0b58c49e8c3d56a9c6
Luciano Mella <lucianomella@gmail.com>
2020-11-26 14:22:42 -0300

    Se agrega index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+var bodyParser  = require('body-parser');
+const PORT = process.env.PORT || 3006;
+let app = express();
+
+app.use(express.json());
+app.use(bodyParser.text({ type: 'text/plain' }))
+
+
+app.listen(PORT);
+console.log(`Express server listening on port ${PORT}`);
+
+app.on('error', function (e) {
+    // Handle your error here
+    console.log(e);
+});
+module.exports = app
