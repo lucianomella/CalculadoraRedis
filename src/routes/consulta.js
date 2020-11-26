cbc13adea6c2eb00c257ba73bbb3829afbb19115
Luciano Mella <lucianomella@gmail.com>
2020-11-26 16:46:28 -0300

    Se agrega conexion a microservicios de operaciones

--- src/routes/consulta.js
+++ src/routes/consulta.js
@@ -1,25 +1,69 @@
 const utils = require("./utils");
+let request = require('request');
 
-exports.suma = async function(data) {
+const calcSumaURL = process.env.calcSumaURL || "http://localhost:3001/suma";
+const calcRestaURL = process.env.calcRestaURL || "http://localhost:3002/resta";
+const calcMultiURL = process.env.calcMultiURL || "http://localhost:3003/multiplica";
+const calcDivideURL = process.env.calcDivideURL || "http://localhost:3004/divide";
+
+let options = {
+    url: '',
+    method: 'POST',
+    json: true,
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: {}
+}
+
+exports.suma = async function (data) {
+    options.url = calcSumaURL;
+    options.body = data;
     return new Promise(function (resolve, reject) {
-        resolve(1);
+        request.post(options, function (error, response, body) {
+            console.log("entró en servicio suma:");
+            if ('statusCode' in response && response.statusCode == '200') {
+                resolve(String(body))
+            }
+        })
     });
 }
 
-exports.resta = function(data) {
+exports.resta = function (data) {
+    options.url = calcRestaURL;
+    options.body = data;
     return new Promise(function (resolve, reject) {
-        resolve(1);
+        request.post(options, function (error, response, body) {
+            console.log("entró en servicio suma:");
+            if ('statusCode' in response && response.statusCode == '200') {
+                resolve(String(body))
+            }
+        })
     });
 }
 
-exports.multi = function(data) {
+exports.multi = function (data) {
+    options.url = calcMultiURL;
+    options.body = data;
     return new Promise(function (resolve, reject) {
-        resolve(1);
+        request.post(options, function (error, response, body) {
+            console.log("entró en servicio suma:");
+            if ('statusCode' in response && response.statusCode == '200') {
+                resolve(String(body))
+            }
+        })
     });
 }
 
-exports.dividir = function(data) {
+exports.dividir = function (data) {
+    options.url = calcDivideURL;
+    options.body = data;
     return new Promise(function (resolve, reject) {
-        resolve(1);
+        request.post(options, function (error, response, body) {
+            console.log("entró en servicio suma:");
+            if ('statusCode' in response && response.statusCode == '200') {
+                resolve(String(body))
+            }
+        })
     });
 }
